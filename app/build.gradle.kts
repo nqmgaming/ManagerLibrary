7b4eb3b3696cacd9ee7df484ec19355fd8eccc2a
Nguyễn Quang Minh <nguyenquangminh570@gmail.com>
2023-09-27 14:40:44 +0700

    cmt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,56 +1,56 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
-plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
-}
-
-android {
-    namespace = "com.example.managerlibrary"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.managerlibrary"
-        minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        viewBinding = true
-        buildConfig = true
-    }
-}
-
-dependencies {
-    implementation(libs.core.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    implementation("androidx.transition:transition:1.4.1")
-    implementation("de.hdodenhof:circleimageview:3.1.0")
-    implementation("com.github.chivorns:smartmaterialspinner:1.5.0")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
-}
+//@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+//plugins {
+//    alias(libs.plugins.androidApplication)
+//    alias(libs.plugins.kotlinAndroid)
+//}
+//
+//android {
+//    namespace = "com.example.managerlibrary"
+//    compileSdk = 34
+//
+//    defaultConfig {
+//        applicationId = "com.example.managerlibrary"
+//        minSdk = 24
+//        targetSdk = 33
+//        versionCode = 1
+//        versionName = "1.0"
+//
+//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//    }
+//
+//    buildTypes {
+//        release {
+//            isMinifyEnabled = false
+//            proguardFiles(
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
+//        }
+//    }
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_1_8
+//        targetCompatibility = JavaVersion.VERSION_1_8
+//    }
+//    kotlinOptions {
+//        jvmTarget = "1.8"
+//    }
+//    buildFeatures {
+//        viewBinding = true
+//        buildConfig = true
+//    }
+//}
+//
+//dependencies {
+//    implementation(libs.core.ktx)
+//    implementation(libs.appcompat)
+//    implementation(libs.material)
+//    implementation(libs.constraintlayout)
+//    testImplementation(libs.junit)
+//    androidTestImplementation(libs.androidx.test.ext.junit)
+//    androidTestImplementation(libs.espresso.core)
+//    implementation("androidx.transition:transition:1.4.1")
+//    implementation("de.hdodenhof:circleimageview:3.1.0")
+//    implementation("com.github.chivorns:smartmaterialspinner:1.5.0")
+//    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+//    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+//}
